3754d1f6a31509169c078680e62d7200c2579438
tahirgalvez1 <tahir.galvez@hotmail.com>
2026-08-03 17:23:20 -0400

    Initial Commit

---
+++ RootsFitness/RootsFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootsFitnessApp.swift
+//  RootsFitness
+//
+//  Created by Tahir Galvez on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RootsFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
